8814e1e5b147aa93defdac349f2a8f65e3a06a3c
RaidenNomura <59089078+RaidenNomura@users.noreply.github.com>
2023-02-02 17:25:16 +0100

    Add Script lifeManager and damage method

---
+++ Assets/Scripts/lifeManagement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifeManagement : MonoBehaviour
+{
+    #region Exposed
+
+    [SerializeField] private int _maxLifePoint = 100;
+    [SerializeField] private int _lifePoint;
+    [SerializeField] private int _damageValue = 10;
+
+    #endregion
+
+    #region Unity Lifecycle
+
+    private void Awake()
+    {
+        _lifePoint = _maxLifePoint;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region Collisions
+
+    // Collision avec l'ennemmi : on décrémente les points de vie courants du joueur
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Teste si le GameObject avec lequel on est entré en collision porte le tag Enemy
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _lifePoint -= _damageValue;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+
+
+    #endregion
+
+    #region Private & Protected
+
+
+
+    #endregion
+}
