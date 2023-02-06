c69bad3c8836c4da0112284d034829f4512abbb6
RaidenNomura <59089078+RaidenNomura@users.noreply.github.com>
2023-02-06 17:28:34 +0100

    Patch Jump and Add Mouvement player

--- Assets/Scripts/movementManagement.cs
+++ Assets/Scripts/movementManagement.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class movementManagement : MonoBehaviour
 {
     #region Exposed
 
-
+    [SerializeField] float _moveSpeed;
 
     #endregion
 
     #region Unity Lifecycle
+
+    private void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
 
     private void Start()
     {
@@ -19,7 +26,22 @@ public class movementManagement : MonoBehaviour
 
     private void Update()
     {
+        _direction.x = Input.GetAxisRaw("Horizontal") * _moveSpeed;
+    }
 
+    private void FixedUpdate()
+    {
+        _rb2D.velocity = _direction * Time.fixedDeltaTime * 50;
+        //_direction.y = _rb2D.velocity.y;
+
+        if (_direction.x < 0f)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (_direction.x > 0f)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
     }
 
     #endregion
@@ -32,7 +54,9 @@ public class movementManagement : MonoBehaviour
 
     #region Private & Protected
 
-
+    private Rigidbody2D _rb2D;
+    private Vector2 _direction;
 
     #endregion
+
 }
