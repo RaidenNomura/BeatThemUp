b0d272c75f494d861c2185512a87f96a33702584
WilliamAll <wil53@hotmail.fr>
2023-02-07 10:44:59 +0100

    Update PlayerStateMachine, change name at player animator

---
+++ Assets/PlayerStateMachine.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using TMPro;
+using UnityEngine;
+
+enum PlayerStateMode
+{
+    WALK,
+    SPRINT,
+    ATTACK
+}
+
+
+public class PlayerStateMachine : StateMachineBehaviour
+{
+    #region exposed
+
+    [SerializeField]
+    float roll_Duration = 0.5f;
+
+
+    #endregion
+
+
+    #region Unity Lifecyle
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    void Start()
+    {
+        TransitionToState(PlayerStateMode.WALK);
+    }
+    void Update()
+    {
+        OnStateUpdate();
+    }
+    #endregion
+
+    #region Methode
+
+    void OnStateEnter()
+    {
+        switch (_currentState)
+        {
+            case PlayerStateMode.WALK:
+                break;
+            case PlayerStateMode.SPRINT:
+                _animator.SetBool("isSprinting", true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnStateExit()
+    {
+        switch (_currentState)
+        {
+            case PlayerStateMode.WALK:
+                break;
+            case PlayerStateMode.SPRINT:
+                _animator.SetBool("isSprinting", false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnStateUpdate()
+    {
+        switch (_currentState)
+        {
+            case PlayerStateMode.WALK:
+                _animator.SetFloat("MoveSpeedX", Input.GetAxis("Horizontal")); //check the name... maybe is MoveSpeedX
+                _animator.SetFloat("MoveSpeedY", Input.GetAxis("Vertical"));
+                break;
+
+            case PlayerStateMode.SPRINT:
+
+                _animator.SetFloat("MoveSpeedX", Input.GetAxis("Horizontal")); //check the name... maybe is MoveSpeedX
+                _animator.SetFloat("MoveSpeedY", Input.GetAxis("Vertical"));
+
+                if (Input.GetButtonUp("Fire3"))
+                {
+                    TransitionToState(PlayerStateMode.WALK);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void TransitionToState(PlayerStateMode toState)
+    {
+        OnStateExit();
+        _currentState = toState;
+        OnStateEnter();
+    }
+
+    #endregion
+
+
+    #region Private & Protected
+
+    private PlayerStateMode _currentState;
+
+    private Animator _animator;
+
+    private float _endRollTime;
+
+
+    #endregion
+}
